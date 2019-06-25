a78c684a3861835c23f5cc84b116b8e69a137776
Faizanshah Ansari <faizanshah.ansari@gmail.com>
2019-06-25 22:40:16 +0530

    Optimizing. Finalizing comments for Scoring_Algo

--- Game Modules/Scoring_Algo.py
+++ Game Modules/Scoring_Algo.py
@@ -1,16 +1,21 @@
 import math
 import Data
 
+
 #  Scoring Scheme :
 #  2 word link = 3 * ( 1 + 0.9 ) score increment
 #  3 word link = 3 * ( 1 + 0.9 + 0.8 ) score increment
 #  n word link = 3 * ( 1 + 0.9 + 0.8 + ... + ( 1 + ( n - 1 ) * (-0.1) ) ) score increment
 
-def update_score(s, l, stat):
+def update_score(score, val, stat):
+
+    # In this case val = length of the link
     if( stat == 'None' ):
-        increment = 3 * ((l / 2) * (2 + (l - 1) * (-0.1)))
-        return(math.ceil(s + increment))
+        increment = 3 * ((val / 2) * (2 + (val - 1) * (-0.1))) # Sum up to n terms of an A.P.
+        return(math.ceil(score + increment))
+
+    # In this case val = Time remaining
     elif( stat == 'Won' ):
-        increment = math.floor((60 - l) * 0.5)
+        increment = math.floor(val * 0.5)
         Data.TimeBonus = increment
-        return(s + increment)
+        return(score + increment)
