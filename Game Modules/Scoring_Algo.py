afab4ba63dbc59cc49f9e6d9fd27bf1dacba0d3c
Faizanshah Ansari <faizanshah.ansari@gmail.com>
2019-06-25 22:20:42 +0530

    Inspection Update

--- Game Modules/Scoring_Algo.py
+++ Game Modules/Scoring_Algo.py
@@ -1,7 +1,7 @@
-from math import *
+import math
 import Data
 
-#Scoring Scheme :
+#  Scoring Scheme :
 #  2 word link = 3 * ( 1 + 0.9 ) score increment
 #  3 word link = 3 * ( 1 + 0.9 + 0.8 ) score increment
 #  n word link = 3 * ( 1 + 0.9 + 0.8 + ... + ( 1 + ( n - 1 ) * (-0.1) ) ) score increment
@@ -9,8 +9,8 @@ import Data
 def update_score(s, l, stat):
     if( stat == 'None' ):
         increment = 3 * ((l / 2) * (2 + (l - 1) * (-0.1)))
-        return(ceil(s + increment))
+        return(math.ceil(s + increment))
     elif( stat == 'Won' ):
-        increment = floor((60 - l) * 0.5)
+        increment = math.floor((60 - l) * 0.5)
         Data.TimeBonus = increment
         return(s + increment)
