f1643b4063a062250317930d0a1164ba85ea2c6d
Faizanshah Ansari <faizanshah.ansari@gmail.com>
2019-06-26 15:41:58 +0530

    Inspection Update

--- Game Modules/freq_alpha.py
+++ Game Modules/freq_alpha.py
@@ -1,21 +1,21 @@
 import string
 
+
 selected_chr = ''
 
+
 def anag_freq_alpha_selector(anaglinks, ignore_list):
-
     global selected_chr
-    
-    def freq_alpha_out(Input, mode = 0):
 
+    def freq_alpha_out(Input, mode = 0): # mode = 0 gives a list of selectable alphabets; mode = 1 gives frequent alphabets belonging to items in the ingnore_list
         count_dict = {alpha: 0 for alpha in string.ascii_uppercase}
-        
+
         for anaglink in Input:
             for wrd in anaglink:
                 for alpha in wrd:
                     count_dict[alpha.upper()] += 1
 
-        result = sorted(count_dict.items(), reverse = bool(mode), key = lambda tup : tup[1])
+        result = sorted(count_dict.items(), reverse = bool(mode), key = lambda tup : tup[1]) # Sort the dict items according to their values in ascending/descending order according to the mode
 
         if(mode == 0):
             temp = list()
@@ -28,7 +28,7 @@ def anag_freq_alpha_selector(anaglinks, ignore_list):
 
     Ans_alpha = freq_alpha_out(anaglinks)
     nonAns_alpha = freq_alpha_out(ignore_list, 1)
-
+    ##INSPECTING
     selected = ('', 51)
     for i in range(len(Ans_alpha)):
         score = i + nonAns_alpha.index(Ans_alpha[i])
@@ -41,12 +41,11 @@ def anag_freq_alpha_selector(anaglinks, ignore_list):
 
     return selected[0]
 
+
 def run(anaglinks):
 
     global selected_chr
-    
-    for i in range(len(anaglinks)):
-        if(selected_chr not in anaglinks[i][0].upper() and selected_chr != ''): 
-            anaglinks[i].append(-1)
 
-    
+    for i in range(len(anaglinks)):
+        if(selected_chr not in anaglinks[i][0].upper() and selected_chr != ''):
+            anaglinks[i].append(-1)
