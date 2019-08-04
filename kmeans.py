394635ac540c91597c3971397619f56380d3871d
Faizanshah Ansari <faizanshah.ansari@gmail.com>
2019-08-04 17:39:11 +0530

    Updating kmeans module

--- kmeans.py
+++ kmeans.py
@@ -1,15 +1,23 @@
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()  # for plot styling
 import numpy as np
-X=np.loadtxt("kmeans.txt",delimiter=',')
-from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=5)
+
+
+X = np.loadtxt("kmeans.txt", delimiter = ',')
+
+kmeans = KMeans(n_clusters = 5)
 kmeans.fit(X)
+
 labels = kmeans.predict(X)
-#print(labels)
-centers=kmeans.cluster_centers_
-#print(centers)
-Y=np.loadtxt("kmeans.txt",delimiter=',')
-i=kmeans.predict(Y)
-plt.scatter(X[:,0],X[:,1], c=labels, cmap='rainbow')  
+
+centers = kmeans.cluster_centers_
+
+'''
+# For predecting current result's cluster
+Y = np.loadtxt("kmeans.txt", delimiter = ',')
+i = kmeans.predict(Y)
+'''
+
+plt.scatter(X[ : , 0], X[ : , 1], c = labels, cmap = 'rainbow')  
 plt.show()
